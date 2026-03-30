d131c2ff1a3153984f1a6a342d0959f417ba6058
Marlon9511 <marlonkurth79@gmail.com>
2026-03-31 01:38:28 +0200

    Aktualisieren von index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } from '@whiskeysockets/baileys';
+import { makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } from '@717Development/baileys';
 import fs from 'fs';
 import path from 'path';
 import P from 'pino';
