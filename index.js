132eeab0908f470386fb8089059728a90812c2d7
Marlon9511 <marlonkurth79@gmail.com>
2026-08-11 04:12:15 +0200

    Aktualisieren von index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ import { createAuthTools } from './web-auth.js';
 import { createGameRoutes } from './web-games.js';
 import { createGuildBossSystem } from './guildboss-event.mjs';
 import { createGuildWarSystem } from './guildwars.mjs';
+import { createGuildWarSystem, GUILDWAR_COMMANDS, GUILDWAR_HELP_TEXT } from './guildwars.mjs';
 
 // ========== GLOBALE FEHLERABSICHERUNG ==========
 process.on('unhandledRejection', (reason) => {
